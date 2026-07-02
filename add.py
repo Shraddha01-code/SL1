f0a853127939e373669433f91c8fef5ce7d99783
Shraddha01-code <shraddha.codes.ai@gmail.com>
2026-07-02 14:17:50 +0530

    Update add.py

--- add.py
+++ add.py
@@ -4,6 +4,6 @@ def add(a1, a2):
 def multiply(a1,a2):
   return a1 * a2
 
-a,b = map(int, input("Enter two numbers for addition: ").split())
-print(add(a,b))
-print(multiply(a, b))
+a,b = map(int, input("Enter two numbers: ").split())
+print("Addition is ",add(a,b))
+print("Multiplication is ",multiply(a, b))
