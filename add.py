eab12154bc25034c76f5e29b1aeab26b384190a5
Shraddha <shraddha.codes.ai@gmail.com>
2026-07-02 14:12:24 +0530

    multipication added

--- add.py
+++ add.py
@@ -1,5 +1,9 @@
 def add(a1, a2):
   return a1 + a2
 
+def multiply(a1,a2):
+  return a1 * a2
+
 a,b = map(int, input("Enter two numbers for addition: ").split())
 print(add(a,b))
+print(multiply(a, b))
